c72fa95eeb31d065124e8a66756b934259076638
Nokinen <tobias.noiges@gmail.com>
2010-12-13 12:02:15 +0100

    Initial commit

---
+++ CocoaMiniLogoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  CocoaMiniLogoAppDelegate.h
+//  CocoaMiniLogo
+//
+//  Created by Nokinen on 13.12.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CocoaMiniLogoAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CocoaMiniLogoAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  CocoaMiniLogoAppDelegate.m
+//  CocoaMiniLogo
+//
+//  Created by Nokinen on 13.12.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "CocoaMiniLogoAppDelegate.h"
+
+@implementation CocoaMiniLogoAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
